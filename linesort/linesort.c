44ed4a66297714e35dc91febb4f4bb0ca364c75c
Hai Vo <haivo@outlook.com>
2012-12-04 01:29:27 -0800

    hello

---
+++ linesort/linesort.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   linesort.c
+ * Author: Hai Vo
+ *
+ * Created on December 4, 2012, 1:29 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
